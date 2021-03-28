4f1fdbc916f8919313705dfc4d66ac6e85f798ed
Haixiang <haixiang6123@gmail.com>
2021-03-28 14:43:43 +0800

    feat: 添加自定义复制例子

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import copy from './lib/index'
 
 const $copy = document.querySelector<HTMLButtonElement>('#copy')
+const $myCopy = document.querySelector<HTMLButtonElement>('#my-copy')
 const $text = document.querySelector<HTMLParagraphElement>('#text')
 
 $copy.onclick = () => {
   const copyText = $text.textContent
 
-  copy(copyText, {
-    onCopy: (data) => data.setData('text/plain', 'foo'),
+  copy(copyText)
+}
+
+$myCopy.onclick = () => {
+  const myText = document.querySelector<HTMLParagraphElement>('#my-text').innerText
+
+  copy('xxx', {
+    onCopy: (data) => data.setData('text/plain', myText),
   })
 }
